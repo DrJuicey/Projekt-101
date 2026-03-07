d3b8b947687f2900dcd72d0d802b6cde246eee1a
Joen <joenberisha99@gmail.com>
2026-03-07 15:35:03 +0100

    train.py

--- src/train.py
+++ src/train.py
@@ -1,1 +1,12 @@
+import torch
+from torchvision import datasets, transforms
 
+# MNIST dataset laden
+train_dataset = datasets.MNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=transforms.ToTensor()
+)
+
+print("MNIST dataset loaded")
